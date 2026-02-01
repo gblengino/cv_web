01c595b76171635863ab2c8186eb896d4cc38858
gblengino <g.blengino@itecriocuarto.org.ar>
2026-01-31 21:23:35 -0300

    Added base Layout: Sidebar with Header and Nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        mdi: ["*"], // Material Design Icons (UX/UI)
+        "simple-icons": ["*"] // Simple Icons (Logos)
+      }
+    })
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
